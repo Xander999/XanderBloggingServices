81d580d1908f8d9afbc700f326d930ea9cbe3fe4
sahasuraj <sahasuraj420@gmail.com>
2024-10-30 08:56:40 +0530

    FirstCommit: Setup the Project

---
+++ src/main/java/com/xanderProjects/blog/XanderBlogging/XanderBloggingApplication.java
@@ -0,0 +1,13 @@
+package com.xanderProjects.blog.XanderBlogging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XanderBloggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XanderBloggingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xanderProjects/blog/XanderBlogging/XanderBloggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xanderProjects.blog.XanderBlogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XanderBloggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
